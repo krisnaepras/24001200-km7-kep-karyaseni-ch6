255d5dd24e793e25f57eb6411c83f22f4dd98f60
krisnaepras <161457644+krisnaepras@users.noreply.github.com>
2024-11-10 15:50:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Halo gaiss welkom!')
+    res.send('Halo gaiss welkombek!')
 })
 
 app.use("/api/v1", router);
